207f87e6898c0aed99a8f5773fdeaac8089fa933
sanoshchenko <sanoshchenko@cogniance.com>
2016-02-04 17:23:23 +0200

    no message

--- src/main/java/org/perlovka/parc/Application.java
+++ src/main/java/org/perlovka/parc/Application.java
@@ -1,13 +1,8 @@
 package org.perlovka.parc;
 
 import org.perlovka.parc.merchant.MerchantGenerator;
-import org.perlovka.parc.offer.OfferGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-/**
- * Created by sanoshchenko on 1/31/16.
- */
 public class Application {
 
   private static final Logger LOG = LoggerFactory.getLogger(Application.class);
@@ -15,11 +10,6 @@ public class Application {
   public static void main(String[] args) throws Exception {
     LOG.debug("Application started.");
     MerchantGenerator.get().generateMerchants();
-//    OfferGenerator offerGenerator = OfferGenerator.get();
-//    for (int i = 0; i < 10; i++) {
-//      LOG.debug(offerGenerator.generateOffer(i, "FOOD"));
-//    }
-
     LOG.debug("Application finished.");
   }
 
